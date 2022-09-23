c75b71eda88d5d544cdfc49635c60355d957350b
Angelinsky7 <steven.demarco@while-not-false.ch>
2022-09-23 11:36:44 +0200

    fix(ui): menu hidden in projects view

--- init.rb
+++ init.rb
@@ -8,10 +8,20 @@ Redmine::Plugin.register :redmine_time_statistics do
   url 'https://github.com/Angelinsky7/redmine_time_statistics.git'
   author_url 'https://github.com/Angelinsky7/redmine_time_statistics.git'
 
+  def permission_checker(permission_list)
+    proc {
+      flag = false
+      permission_list.each { |permission|
+        flag ||= User.current.allowed_to_globally?(permission, {})
+      }
+      flag
+    }
+  end
+
   project_module :redmine_time_statistics do
     permission :view_time_statistics, :time_statistics => :index
 
-    menu :application_menu, :time_statistics, { :controller => 'time_statistics', :action => 'index' }, :caption => :label_time_statistics
+    menu :application_menu, :time_statistics, { :controller => 'time_statistics', :action => 'index' }, :caption => :label_time_statistics, :if => permission_checker([:view_time_statistics])
     menu :project_menu, :time_statistics, { :controller => 'time_statistics', :action => 'index' }, :caption => :label_time_statistics, :param => :project_id
   end
  
